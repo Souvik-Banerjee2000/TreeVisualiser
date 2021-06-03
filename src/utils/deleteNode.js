aa6e8f02938f87329edfab78c6c12cd77f460079
Souvik Banerjee <souvik.banerjee@sci-bi.com>
2021-06-03 11:23:09 +0530

    Delete Done finally

--- src/utils/deleteNode.js
+++ src/utils/deleteNode.js
@@ -1,38 +1,22 @@
 let isFound = true;
 
 
-function findParent(greatest,root){
-
-    // console.log(root);
-
-
-
-    // if( root.children.length == 1 && root.attributes.left === '' && root.children[0].name === greatest.name)
-    //     return root
-    // else if(root.children.length >1 && root.children[1].name === greatest.name ) 
-    //     return root;
-    // else if(root.children.length == 1 && root.attributes.left === '')
-    //     return findParent(root.children[0]);
-    // else if(root.children.length >1)
-    //     return findParent(root.children[1]);  
-        
-
-}
-
-function findGreatest(root){
+function findGreatest(root,parent){
 
     if(root.attributes.right!==''){
         if(root.attributes.left!==''){
-            return findGreatest(root.children[1]);
+            parent = root;
+            return findGreatest(root.children[1],parent);
         }
-        return findGreatest(root.children[0]);
+        parent = root;
+        return findGreatest(root.children[0],parent);
     }
-    return root;
+    return [root,parent];
 
 }
 
 
-function search(root,val){
+function del(root,val){
     if(root.children[0].name === val){
         console.log(root.children);
         if(root.children[0].attributes.left === '' && root.children[0].attributes.right === ''){ //leaf node
@@ -44,7 +28,7 @@ function search(root,val){
             }
             root.children.shift();
             // console.log(root);
-        }else if(root.children[0].attributes.left === '' && root.children[0].attributes.right !== ''){
+        }else if(root.children[0].attributes.left === '' && root.children[0].attributes.right !== ''){ //left does not exist but right do
             // console.log(root);
             root.children[0] = root.children[0].children[0];
             // console.log(root);
@@ -53,11 +37,36 @@ function search(root,val){
                 root.children[0] = root.children[0].children[0];
             }
             else{
-                console.log(root.children[0]);
-                let greatest = findGreatest(root.children[0].children[0]);
-                let parent = findParent(greatest,root.children[0].children[0]);
-                console.log(greatest);
-                console.log(parent);
+                // console.log(root.children[0]);
+                let [greatest,parent] = findGreatest(root.children[0].children[0],root.children[0]);
+                // let parent = findParent(greatest,root.children[0].children[0]);
+
+                console.log(greatest,parent);
+
+                if(parent !== root.children[0]){
+                    parent.attributes.right = greatest.attributes.left;
+                    let v = greatest.children;
+                    console.log(v);
+                    parent.children.pop();
+                    parent.children.push(...v);
+                }else{
+                    parent.attributes.left = greatest.attributes.left;
+                    let v = greatest.children;
+                    parent.children.shift();
+                    parent.children.unshift(...v);
+                }
+
+
+                root.children[0].name = greatest.name;
+
+
+                // console.log("Hello");
+                // console.log(state);
+                
+                
+                // parent.children[1] = greatest.children[0];
+                // console.log(greatest);
+                // console.log(parent);
             }
 
 
@@ -67,24 +76,49 @@ function search(root,val){
             root.attributes.right = '';
             root.children.pop();
         }
-        else if(root.children[1].attributes.left === '' && root.children[1].attributes.right !== ''){
-            // console.log(root.children[1]);
+        else if(root.children[1].attributes.left === '' && root.children[1].attributes.right !== ''){ //left does not exist but right do
+            
             root.children[1] = root.children[1].children[0];
-            // console.log(root.children[1].children[0]);
+            
+        }
+        else if(root.children[1].attributes.left !== ''){ //left exists
+            if(root.children[1].attributes.right === ''){ //right does not
+                root.children[1] = root.children[1].children[0];
+            }
+            else{ //right also exists with left
+
+
+                let [greatest,parent] = findGreatest(root.children[1].children[0],root.children[1]);
+                
+                if(parent !== root.children[1]){
+                    parent.attributes.right = greatest.attributes.left;
+                    let v = greatest.children;
+                    console.log(v);
+                    parent.children.pop();
+                    parent.children.push(...v);
+                }else{
+                    parent.attributes.left = greatest.attributes.left;
+                    let v = greatest.children;
+                    parent.children.shift();
+                    parent.children.unshift(...v);
+                }
+                root.children[1].name = greatest.name;
+            }
+
         }
     }
     else if(root.name>val){
         if(root.attributes.left !== ""){
-            search(root.children[0],val)
+            del(root.children[0],val)
         }else{
             isFound = false;
         }
     }else if(root.name < val){
         if(root.attributes.right !== ""){
             if(root.children.length >1)
-                search(root.children[1],val);
+                del(root.children[1],val);
             else
-                search(root.children[0],val);    
+                del(root.children[0],val);    
         }else{
             isFound = false;
         }
@@ -94,15 +128,51 @@ function search(root,val){
 export const deleteNode = (state,input)=>{
 
     // console.log(state);
-    search(state,Number(input));
+    if(Number(state.name) === Number(input)){
+        if(state.attributes.left === '' && state.attributes.right === ''){
+            return {};
+        } 
+        else if(state.attributes.left === '' && state.attributes.right!==''){
+            // console.log(state.children);
+            state = state.children[0];    
+            // console.log(state.children);
+        }else if(state.attributes.left!==''){
+            if(state.attributes.right === ''){
+                // console.log(state.children);
+                state = state.children[0];
+                // console.log(state.children);
+            }else{
+                let [greatest,parent] = findGreatest(state.children[0],state);
+                // let parent = findParent(greatest,root.children[0].children[0]);
+
+                console.log(greatest,parent);
+
+                if(parent !== state){
+                    parent.attributes.right = greatest.attributes.left;
+                    let v = greatest.children;
+                    console.log(v);
+                    parent.children.pop();
+                    parent.children.push(...v);
+                }else{
+                    parent.attributes.left = greatest.attributes.left;
+                    let v = greatest.children;
+                    parent.children.shift();
+                    parent.children.unshift(...v);
+                }
+
+
+                state.name = greatest.name;
+
+            }
+        }
+
+    }else{
+        del(state,Number(input));
+    }
+
     if(isFound)
         return JSON.parse(JSON.stringify(state));
     else
         return state;    
-    // console.log(state,Number(input));
  
-    // if(!found)
-    //     return state;
-    // else return JSON.parse(JSON.stringify(state));
-    // return state;
 }
