6289f58213b7c48e9852627d13adeaa82f8cf9b9
Souvik Banerjee <souvik.banerjee@sci-bi.com>
2021-06-03 21:07:10 +0530

    Orientation Toogler added

--- src/components/TreeComponent.js
+++ src/components/TreeComponent.js
@@ -22,6 +22,7 @@ function TreeComponent() {
     const {treeData,dispatch} = useContext(DataContext);
     const {submission} = useContext(SubmissionContext);
     const [translate, containerRef] = useCenteredTree();
+    const [orientation,setOrientation] = useState("vertical");
 
     // useEffect(()=>{
     //     dispatch({
@@ -34,23 +35,27 @@ function TreeComponent() {
 
     
     return (
-        
-        <div id="treeWrapper" ref={containerRef} style={{ width: '80vw', height: '100vh' }}  >
-            {Object.keys(treeData).length === 0 ?(
-                <></>
-            ):(
-              <Tree 
-                data={treeData}
-                orientation={'vertical'}
-                translate={translate}
-                rootNodeClassName="node__root"
-                branchNodeClassName="node__branch"
-                leafNodeClassName="node__leaf"
-                pathClassFunc={() => 'custom-link'}
+        <div>
+            <div style={{display:'flex', width:'20vw', margin:'0 auto' ,justifyContent:'space-between'}}>
+                <button onClick={()=>{setOrientation("vertical")}}>Vertical</button> <button onClick={()=>{setOrientation("horizontal")}}>Horizontal</button>
+            </div>
+            <div id="treeWrapper" ref={containerRef} style={{ width: '80vw', height: '100vh' }}  >
+                {Object.keys(treeData).length === 0 ?(
+                    <></>
+                ):(
+                <Tree 
+                    data={treeData}
+                    orientation={orientation}
+                    translate={translate}
+                    rootNodeClassName="node__root"
+                    branchNodeClassName="node__branch"
+                    leafNodeClassName="node__leaf"
+                    pathClassFunc={() => 'custom-link'}
 
-            />
-            )}
-            
+                />
+                )}
+                
+            </div>
         </div>
         
     )
